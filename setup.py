11a8f40241de23351129c4c0eefa5a1bd2c38cc9
kamilcodee <125295308+kamilcodee@users.noreply.github.com>
2023-02-19 21:20:04 +0000

    prepare for release

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='rscraper',
+      version='0.1',
+      packages=find_packages(),
+      install_requires=required)
